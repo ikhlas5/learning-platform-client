a9f5188cdd02549eb8ee30147df458ad137f1849
ikhlas5 <mohammadikhlas765@gmail.com>
2022-10-28 23:13:35 +0600

    elliven commit

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -53,6 +53,6 @@ export const router = createBrowserRouter([
                 path:'*',
                 element:<div className="text-red-600  text-center" >Ops! Sorry Page not found:404!</div>
               }
-        ]
+        ]  
     }
 ])
